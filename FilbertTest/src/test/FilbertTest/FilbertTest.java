aed991834392365923b57d41cb9c9342e6a8d3f4
Filbert Hansel <FiL@filberts.local>
2011-02-26 19:48:19 -0800

    FilbertTest

---
+++ FilbertTest/src/test/FilbertTest/FilbertTest.java
@@ -0,0 +1,13 @@
+package test.FilbertTest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FilbertTest extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
