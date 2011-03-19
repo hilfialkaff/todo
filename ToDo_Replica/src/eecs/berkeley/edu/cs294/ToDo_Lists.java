e1b41137c7e9056e37c2e4268001a419ff80b1f8
syjxxii <dicz.hack@gmail.com>
2011-03-18 17:15:29 -0700

    fix bug at todo_lists

--- ToDo_Replica/src/eecs/berkeley/edu/cs294/ToDo_Lists.java
+++ ToDo_Replica/src/eecs/berkeley/edu/cs294/ToDo_Lists.java
@@ -3,10 +3,11 @@ package eecs.berkeley.edu.cs294;
 import java.util.List;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.LinearLayout;
+import android.view.View.OnClickListener;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TableRow.LayoutParams;
@@ -19,35 +20,34 @@ public class ToDo_Lists extends Activity {
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-		tl_todo_lists = (TableLayout) findViewById(R.id.tl_todo_lists);
-		this.dh = new DatabaseHelper(this);
-		
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
+		setContentView(R.layout.todo_lists);
 		
+		tl_todo_lists = (TableLayout) findViewById(R.id.tl_todo_lists);
+		this.dh = new DatabaseHelper(this);	
 		
 		List<String> titles = this.dh.selectAll_to_do_title();
-		
+		Log.w("debug", "titles size " + Integer.toString(titles.size()));
+
 		for (String title : titles) {
 			TableRow row = new TableRow(this);		
 			row.setLayoutParams(new TableLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
 			
-			LinearLayout ll_title = new LinearLayout(this);
-			ll_title.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
-			
 			TextView tv_title = new TextView(this);
-			tv_title.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+			tv_title.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));			
 			tv_title.setText(title);
+			tv_title.setTextSize(40);
 			
-			View ruler = new View(this);
-			ruler.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT, 1));
+			row.addView(tv_title);
+			row.setOnClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					Intent intent = new Intent(v.getContext(), Add.class);
+					startActivityForResult(intent, 2);
+				}
+			});
 			
-			ll_title.addView(tv_title);
-			row.addView(ll_title);
-
 			tl_todo_lists.addView(row);
-			tl_todo_lists.addView(ruler);
-			Log.w("DEBUG", "ONCEEEE");
 		}
 		
 	}
