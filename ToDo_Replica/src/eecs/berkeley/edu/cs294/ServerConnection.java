4185df15c8f34a58141c956b816493239034ca0a
Hilfi Alkaff <hilfia@eecs.berkeley.edu>
2011-04-12 21:40:20 -0700

    Able to actually parse the xml and put it into a list of TODOs

--- ToDo_Replica/src/eecs/berkeley/edu/cs294/ServerConnection.java
+++ ToDo_Replica/src/eecs/berkeley/edu/cs294/ServerConnection.java
@@ -1,11 +1,13 @@
 package eecs.berkeley.edu.cs294;
 
+/* Should not have android.sax.element */
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.StringReader;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -16,7 +18,6 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.w3c.dom.CharacterData;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
@@ -25,28 +26,27 @@ import org.xml.sax.InputSource;
 import android.app.Activity;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.sax.Element;
 import android.util.Log;
 
 public class ServerConnection extends Activity {
-	
+
 	/*
 	 * Check if phone is connected to the internet
 	 */
 	public boolean isConnected() {
 		ConnectivityManager connManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
 		NetworkInfo netInfo = connManager.getActiveNetworkInfo();
-		
+
 		if(netInfo == null) {
 			Log.d("ServerDEBUG", "--------------- No internet connection --------- ");
 			return false;
 		}
-			
+
 		if (netInfo.isConnected()) {
 			Log.d("ServerDEBUG", "------------- Connected to internet -------------");
 			return true;
 		}
-		
+
 		return false;
 	}
 
@@ -54,43 +54,12 @@ public class ServerConnection extends Activity {
 	 * Pull server changes to local and synchronize it with the local database 
 	 */
 	public static void pullRemote() {
-		Log.d("ServerDEBUG", "pullRemote()");
-		retreiveUrl();
-	}
-	
-	/*
-	 * Push local changes to the server
-	 */
-	public static void pushRemote(List<String> oldEntry, List<String> newEntry) {
-	
-		if(oldEntry == null) {
-			Log.d("ServerDEBUG", "old: null");
-		}
-		else {
-			Log.d("ServerDEBUG", "old: " + oldEntry.toString());
-		}	
-	
-		if(newEntry == null) {
-			Log.d("ServerDEBUG", "new: null");
-		}
-		else {
-			Log.d("ServerDEBUG", "new: " + newEntry.toString());
-		}
-	
-		// TODO: push to remote database
-	}
-
-	private static ArrayList<MyTodo> eventsArrayList = null;
-
-	private static void retreiveUrl()
-	{
 		HttpClient httpClient = new DefaultHttpClient();
-
 		String xmlResponse;
 
 		try
 		{
-			// FOR LOCAL DEV: String url = "http://192.168.0.186:3000/events?format=xml";
+			// For localhost use ip 10.0.2.2
 			String url = "http://10.0.2.2:3000/posts?format=xml";
 			Log.d( "ServerDEBUG", "performing get " + url );
 
@@ -100,7 +69,7 @@ public class ServerConnection extends Activity {
 			{
 				xmlResponse = getResponse(response.getEntity());
 				Log.d( "ServerDEBUG", "received " + xmlResponse);
-				// eventsArrayList = parseXMLString(xmlResponse);
+				todoList = parseXMLString(xmlResponse);
 			}
 			else
 			{
@@ -111,9 +80,33 @@ public class ServerConnection extends Activity {
 		} catch (URISyntaxException e) {
 			Log.e( "Error", "URISyntaxException " + e.getMessage() );
 		}
-
 	}
 
+	/*
+	 * Push local changes to the server
+	 */
+	public static void pushRemote(List<String> oldEntry, List<String> newEntry) {
+
+		if(oldEntry == null) {
+			Log.d("ServerDEBUG", "old: null");
+		}
+		else {
+			Log.d("ServerDEBUG", "old: " + oldEntry.toString());
+		}	
+
+		if(newEntry == null) {
+			Log.d("ServerDEBUG", "new: null");
+		}
+		else {
+			Log.d("ServerDEBUG", "new: " + newEntry.toString());
+		}
+
+		// TODO: push to remote database
+	}
+
+	/*
+	 * Get an xml response from the server
+	 */
 	private static String getResponse( HttpEntity entity )
 	{
 		String response = "";
@@ -140,57 +133,63 @@ public class ServerConnection extends Activity {
 	}
 
 	/*
+	 * Parse the XML from the server into a list of TODOs
+	 * 
+	 * TODO: Shouldn't be any hardcoding; Need to discuss w/ the actual server implementation
+	 */
 	private static ArrayList<MyTodo> parseXMLString(String xmlString) {
-		
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+
 		try {
-			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-			DocumentBuilder db = dbf.newDocumentBuilder();
+			DocumentBuilder builder = factory.newDocumentBuilder();
 			InputSource is = new InputSource();
+
 			is.setCharacterStream(new StringReader(xmlString));
+			Document doc = builder.parse(is);
+			NodeList nodes = doc.getElementsByTagName("post");
+			todoList = new ArrayList<MyTodo>();
+			
+			Log.d("ServerDEBUG", "Begin iterating nodes");
 
-			Document doc = db.parse(is);
-			NodeList nodes = doc.getElementsByTagName("myevent");
-
-			eventsArrayList = new ArrayList<MyTodo>(); //Gertig
-
-			//Log.e("Gertig","There are "+nodes.getLength()+" events in this list");
-
-			//Iterate the events
+			/* Iterating each todo node in the xml */
 			for (int i = 0; i < nodes.getLength(); i++) {
+				Log.d("ServerDEBUG", "iteration " + i);
 
-				Element element = (Element) nodes.item(i);
-				eventsArrayList.add(new MyTodo());
+				Node item = nodes.item(i);
+				NodeList properties = item.getChildNodes();
+				todoList.add(new MyTodo());
 
-				NodeList eventIDNum = element.getElementsByTagName("id");
-				Element line = (Element) eventIDNum.item(0);
-				eventsArrayList.get(i).eventID = Integer.parseInt(getCharacterDataFromElement(line));
+				/* Iterating each field (ie: name, content, etc) in a todo */
+				for (int j=0;j<properties.getLength();j++){
 
-				NodeList eventName = element.getElementsByTagName("name");
-				line = (Element) eventName.item(0);
-				eventsArrayList.get(i).name = getCharacterDataFromElement(line).trim();
+					Node property = properties.item(j);
+					String name = property.getNodeName();
 
-
-				NodeList eventBudget = element.getElementsByTagName("budget");
-				line = (Element) eventBudget.item(0);
-				eventsArrayList.get(i).budget = Double.parseDouble(getCharacterDataFromElement(line));
-
+					if (name.equalsIgnoreCase("name")){
+						todoList.get(i).setTodoName((property.getFirstChild().getNodeValue()));
+					} else if (name.equalsIgnoreCase("title")){
+						todoList.get(i).setTodoTitle((property.getFirstChild().getNodeValue()));
+					} else if (name.equalsIgnoreCase("content")){
+						todoList.get(i).setTodoContent((property.getFirstChild().getNodeValue()));
+					}
+				}
 			}
-
 		}
+
 		catch (Exception e) {
 			e.printStackTrace();
 		}
 
-		return eventsArrayList;
-
+		/* For debugging purpose */
+		for(Iterator<MyTodo> it = todoList.iterator(); it.hasNext();) {
+			MyTodo todo = it.next();
+			Log.d("ServerDEBUG", "name: " + todo.getTodoName() + 
+					" title: " + todo.getTodoTitle() + 
+					" content: " + todo.getTodoContent());
+		}
+		
+		return null;
 	}
 
-	public static String getCharacterDataFromElement(Element e) {
-		Node child = e.getFirstChild();
-		if (child instanceof CharacterData) {
-			CharacterData cd = (CharacterData) child;
-			return cd.getData();
-		}
-		return "?"; //ListActivity will display a ? if a null value is passed to the Rails server
-	}*/
+	private static ArrayList<MyTodo> todoList;
 }
