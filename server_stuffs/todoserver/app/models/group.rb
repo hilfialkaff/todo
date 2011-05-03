228ea87d0119429719fee14b963fc2357df87a47
Hilfi Alkaff <hilfia@eecs.berkeley.edu>
2011-05-03 11:05:43 -0700

    - Validates entries in the db tables - Patches tododetail views

--- server_stuffs/todoserver/app/models/group.rb
+++ server_stuffs/todoserver/app/models/group.rb
@@ -1,4 +1,9 @@
 class Group < ActiveRecord::Base
   has_and_belongs_to_many :users
   has_many :tododetails, :dependent => :destroy
+
+  validates :name, :presence => true
+  validates_uniqueness_of :name, :scope => :id
+
+  attr_accessible :name
 end
