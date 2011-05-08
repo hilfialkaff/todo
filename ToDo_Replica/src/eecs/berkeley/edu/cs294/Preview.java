4926a41375bd99b4db0a1333e14f39366ca3b0b7
Andhika Joe <evie_xiii@Andhika-Joes-MacBook-Pro.local>
2011-05-07 17:52:00 -0700

    preview garden now works

--- ToDo_Replica/src/eecs/berkeley/edu/cs294/Preview.java
+++ ToDo_Replica/src/eecs/berkeley/edu/cs294/Preview.java
@@ -4,23 +4,15 @@ import java.util.List;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.AnalogClock;
+import android.widget.Button;
 import android.widget.TabHost;
-import android.widget.TextView;
 import android.widget.TabHost.TabSpec;
+import android.widget.TextView;
 
 public class Preview extends Activity {
-	/** Called when the activity is first created. */
-	TextView tv_title_preview2;
-	TextView tv_place_preview2;
-	TextView tv_note_preview2;
-	TextView tv_tag_preview2;
-	TextView tv_group_preview2;
-	TextView tv_status_preview2;
-	TextView tv_priority_preview2;
 	TabHost th_preview;
+	TextView tv_title_2, tv_place_2, tv_note_2, tv_tag_2, tv_group_2, tv_status_2, tv_priority_2, tv_deadline_2;
+	Button b_ok;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -31,44 +23,40 @@ public class Preview extends Activity {
 		th_preview.setup();
 
 		TabSpec tab_one = th_preview.newTabSpec("tab_one_btn_tab");
-		tab_one.setContent(R.id.tl_preview_1);
+		tab_one.setContent(R.id.tl_preview_21);
 		tab_one.setIndicator("Basic");
 		th_preview.addTab(tab_one);
 
 		TabSpec tab_two = th_preview.newTabSpec("tab_two_btn_tab");
-		tab_two.setContent(R.id.tl_preview_2);
-		tab_two.setIndicator("Advanced");
+		tab_two.setContent(R.id.tl_preview_22);
+		tab_two.setIndicator("More");
 		th_preview.addTab(tab_two);
-
+		
 		th_preview.setCurrentTab(0);
-
-		ToDo_Replica.dh = new DatabaseHelper(this);
-
-		tv_title_preview2 = (TextView) findViewById(R.id.tv_title_preview2);
-		tv_place_preview2 = (TextView) findViewById(R.id.tv_place_preview2);
-		tv_note_preview2 = (TextView) findViewById(R.id.tv_note_preview2);
-		tv_tag_preview2 = (TextView) findViewById(R.id.tv_tag_preview2);
-		tv_group_preview2 = (TextView) findViewById(R.id.tv_group_preview2);
-		tv_status_preview2 = (TextView) findViewById(R.id.tv_status_preview2);
-		tv_priority_preview2 = (TextView) findViewById(R.id.tv_priority_preview2);
-
+		
+		tv_title_2 = (TextView) findViewById(R.id.tv_title_2);
+		tv_place_2 = (TextView) findViewById(R.id.tv_place_2);
+		tv_note_2 = (TextView) findViewById(R.id.tv_note_2);
+		tv_tag_2 = (TextView) findViewById(R.id.tv_tag_2);
+		tv_group_2 = (TextView) findViewById(R.id.tv_group_2);
+		tv_status_2 = (TextView) findViewById(R.id.tv_status_2);
+		tv_priority_2 = (TextView) findViewById(R.id.tv_priority_2);
+		tv_deadline_2 = (TextView) findViewById(R.id.tv_deadline_2);
+		
 		Bundle extras = getIntent().getExtras();
-		if (extras == null) {
-			Log.w("debug", "extras null");
-			return;
-		}
 
 		String title = extras.getString("title_select");
 		List<String> row = ToDo_Replica.dh.select_to_do("title", title);
 
 		if(!row.isEmpty()){
-			tv_title_preview2.setText(row.get(0));
-			tv_place_preview2.setText(row.get(1));
-			tv_note_preview2.setText(row.get(2));
-			tv_tag_preview2.setText(row.get(3));
-			tv_group_preview2.setText(row.get(4));
-			tv_status_preview2.setText(row.get(5));
-			tv_priority_preview2.setText(row.get(6));
+			tv_title_2.setText(row.get(0));
+			tv_place_2.setText(row.get(1));
+			tv_note_2.setText(row.get(2));
+			tv_tag_2.setText(row.get(3));
+			tv_group_2.setText(row.get(4));
+			tv_status_2.setText(row.get(5));
+			tv_priority_2.setText(row.get(6));
+			tv_deadline_2.setText(row.get(7));
 		}
 	}
 }
